b622800100522ab2835c4751f09e2bf2a4776779
m-maheu-giroux <mathieu.maheu-giroux@mcgill.ca>
2019-04-29 14:53:18 -0400

    Modifying options

--- Test Code.R
+++ Test Code.R
@@ -1,3 +1,11 @@
 
 devtools::load_all("~/Google Drive/McGill/Research/PEPFAR ANC/ANCRTAdjust/R")
 
+data("ancrt")
+
+ancrt <- name_var(ancrt, faciluid = "faciluid", time = "time", n_clients = "n_clients",
+                  n_status = "n_status", knownpos = "knownpos", testpos = "testpos",
+                  testneg = "testneg", age = "age") 
+
+ancrt_cleaned <- data_clean(ancrt)
+descriptive_plot(ancrt_cleaned)
